04cbc43fb8fa1bbbaa2d93cb85514b79f9e788a6
testerkajal <154511406+testerkajal@users.noreply.github.com>
2023-12-22 16:22:22 +0000

    1st session complete

---
+++ Basics/01_variables.js
@@ -0,0 +1,22 @@
+const accountId = 123455
+let email = "kajal@gmail.com"
+var password = "test*123"
+city = "ghaziabad" // in js we can decalare a variable without datatype
+let State; // if we don't assign value to any variable it will be printed as undefined.
+
+//accountId = 23455 //it is not allowed as it is decalred as const above
+
+email = "kajal@kc.com"
+password = "test*1234"
+city = "Noida"
+
+
+console.log ([accountId, email, password, city, State]) 
+
+/* In the modern js programming devs only uses let and const and avoid use of var 
+as Variables declared by let are only available inside the block where they're defined.
+while var can be accessible out of the block in which they decalred. which was an issue. 
+*/
+
+
+
